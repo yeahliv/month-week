ba72c8cec6ac4ddf4cf44eb9960f614994473d52
LivYe <951946936@qq.com>
2020-07-29 16:36:10 +0800

    feat: readme.md

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # month-week
 A tool for generating monthly and weekly data
+
+## Install
+```bash
+npm install month-week --save
+```
+
+## Use
+```javascript
+import {Month, Week} from 'month-week'
+
+const month = new Month()
+const week = new Month()
+
+month.today // Today
+month.year // Year
+month.month // month
+month.curMonthFirstDate() // Return current month first day instance of Date Object
+month.curMonthFirstDay() // Return current month first day
+month.curMonthLastDate() // Return current month last day instance of Date Object
+month.lastMonthLastDate() // Return last month last day instance of Date Object
+month.preMonthDayCount() // Return last month day count
+month.getMonth() // Return current month data
+month.lastMonth() // Return last month data
+month.nextMonth() // Return next month data
+month.resetMonth() // Reset and return current month data
+
+week.today // Today
+week.time // Current time
+week.day() // Return day
+week.isToday() // Check is today
+week.monday() // Return monday instance of Date Object
+week.monLastDay() // Return last sunday instance of Date Object
+week.sunday() // Return sunday instance of Date Object
+week.sunNextDay() // Return next monday instance of Date Object
+week.week() // Return this week data
+week.lastWeek() // Return last week data
+week.nextWeek() // Return next week data
+week.reset() // Reset and return this week data
+```
--- index.js
+++ index.js
@@ -1,35 +1,149 @@
+class Week {
+
+  /**
+   * constructor
+   * @param {String|Number} year 
+   * @param {String|Number} month 
+   * @param {String|Number} date 
+   */
+  constructor(year, month, date) {
+    // Init today
+    this.today = new Date()
+
+    // Init current time
+    if (!year || !month || !date) {
+      this.time = new Date()
+    } else {
+      this.time = new Date(year, month - 1, date)
+    }
+  }
+
+  // Return day
+  day() {
+    return this.time.getDay() === 0 ? 7 : this.time.getDay()
+  }
+
+  // Check is today
+  isToday(time) {
+    let year = this.today.getFullYear()
+    let month = this.today.getMonth()
+    let date = this.today.getDate()
+    let _year = time.getFullYear()
+    let _month = time.getMonth()
+    let _date = time.getDate()
+    return year == _year && month == _month && date == _date ? true : false
+  }
+
+  // Return monday instance of Date Object
+  monday() {
+    return new Date(this.time - (this.day() - 1) * 86400000)
+  }
+
+  // Return last sunday instance of Date Object
+  monLastDay() {
+    return new Date(this.monday() - 86400000)
+  }
+  // Return sunday instance of Date Object
+  sunday() {
+    return new Date((this.monday() / 1000 + 6 * 86400) * 1000)
+  }
+
+  // Return next monday instance of Date Object
+  sunNextDay() {
+    return new Date((this.sunday() / 1000 + 86400) * 1000)
+  }
+
+  // Return this week data
+  week() {
+    let res = []
+    for (let i = 0; i < 7; i++) {
+      let date = new Date(this.sunday() - (6 - i) * 86400000)
+      res.push({
+        year: date.getFullYear(),
+        month: date.getMonth() + 1,
+        date: date.getDate(),
+        day: date.getDay() === 0 ? 7 : date.getDay(),
+        _year: `${date.getFullYear()}`,
+        _month:
+          date.getMonth() + 1 < 10
+            ? `0${date.getMonth() + 1}`
+            : `${date.getMonth() + 1}`,
+        _date: date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`,
+        isToday: this.isToday(date)
+      })
+    }
+    return res
+  }
+
+  // Return last week data
+  lastWeek() {
+    this.time = this.monLastDay()
+    return this.week()
+  }
+
+  // Return next week data
+  nextWeek() {
+    this.time = this.sunNextDay()
+    return this.week()
+  }
+
+  // Reset and return this week data
+  reset(year, month, date) {
+    if (!year || !month || !date) {
+      this.time = new Date()
+    } else {
+      this.time = new Date(year, month - 1, date)
+    }
+    return this.week()
+  }
+}
+
 class Month {
 
+  /**
+   * constructor
+   * @param {String|Number} year 
+   * @param {String|Number} month 
+   */
   constructor(year, month) {
+    // Init today
     this.today = year || month ? new Date(year, month) : new Date()
+    // Init year
     this.year = year ? year : this.today.getFullYear()
+    // Init month
     this.month = month ? month : this.today.getMonth() + 1
   }
 
-  checkSunday(num) {
+  // Return day
+  _checkSunday(num) {
     return num === 0 ? 7 : num
   }
 
+  // Return current month first day instance of Date Object
   curMonthFirstDate() {
     return new Date(this.year, this.month - 1, 1)
   }
-
+  // Return current month first day
   curMonthFirstDay() {
-    return this.checkSunday(this.curMonthFirstDate().getDay())
+    return this._checkSunday(this.curMonthFirstDate().getDay())
   }
 
+  // Return current month last day instance of Date Object
   curMonthLastDate() {
     return new Date(this.year, this.month, 0)
   }
 
+  // Return last month last day instance of Date Object
   lastMonthLastDate() {
     return new Date(this.year, this.month - 1, 0)
   }
 
+  // Return last month day count
   preMonthDayCount() {
     return this.curMonthFirstDay() - 1
   }
 
+  // Return current month data
   getMonth() {
     let res = []
     for (let i = 0; i < 7 * 6; i++) {
@@ -80,106 +194,25 @@ class Month {
     return res
   }
 
+  // Return last month data
   lastMonth() {
     this.month = (this.month - 1) === 0 ? 12 : (this.month - 1)
     this.year = (this.month === 12) ? this.year - 1 : this.year
     return this.getMonth()
   }
 
+  // Return next month data
   nextMonth() {
     this.month = (this.month + 1) === 12 ? 1 : (this.month + 1)
     this.year = (this.month === 1) ? this.year + 1 : this.year
     return this.getMonth()
   }
 
+  // Reset and return current month data
   resetMonth() {
     this.year = this.today.getFullYear()
     this.month = this.today.getMonth() + 1
     return this.getMonth()
-  }
-}
-
-class Week {
-
-  constructor(year, month, date) {
-    this.today = new Date()
-
-    if (!year || !month || !date) {
-      this.time = new Date()
-    } else {
-      this.time = new Date(year, month - 1, date)
-    }
-  }
-
-  day() {
-    return this.time.getDay() === 0 ? 7 : this.time.getDay()
-  }
-
-  // Is it today
-  isToday(time) {
-    let year = this.today.getFullYear()
-    let month = this.today.getMonth()
-    let date = this.today.getDate()
-    let _year = time.getFullYear()
-    let _month = time.getMonth()
-    let _date = time.getDate()
-    return year == _year && month == _month && date == _date ? true : false
-  }
-
-  monday() {
-    return new Date(this.time - (this.day() - 1) * 86400000)
-  }
-
-  monLastDay() {
-    return new Date(this.monday() - 86400000)
-  }
-
-  sunday() {
-    return new Date((this.monday() / 1000 + 6 * 86400) * 1000)
-  }
-
-  sunNextDay() {
-    return new Date((this.sunday() / 1000 + 86400) * 1000)
-  }
-
-  week() {
-    let res = []
-    for (let i = 0; i < 7; i++) {
-      let date = new Date(this.sunday() - (6 - i) * 86400000)
-      res.push({
-        year: date.getFullYear(),
-        month: date.getMonth() + 1,
-        date: date.getDate(),
-        day: date.getDay() === 0 ? 7 : date.getDay(),
-        _year: `${date.getFullYear()}`,
-        _month:
-          date.getMonth() + 1 < 10
-            ? `0${date.getMonth() + 1}`
-            : `${date.getMonth() + 1}`,
-        _date: date.getDate() < 10 ? `0${date.getDate()}` : `${date.getDate()}`,
-        isToday: this.isToday(date)
-      })
-    }
-    return res
-  }
-
-  lastWeek() {
-    this.time = this.monLastDay()
-    return this.week()
-  }
-
-  nextWeek() {
-    this.time = this.sunNextDay()
-    return this.week()
-  }
-
-  reset(year, month, date) {
-    if (!year || !month || !date) {
-      this.time = new Date()
-    } else {
-      this.time = new Date(year, month - 1, date)
-    }
-    return this.week()
   }
 }
 
